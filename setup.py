50f3b56732786f77090a6b33dc22310ecd41af93
aershov <codumentary.com@gmail.com>
2017-01-06 17:05:50 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='github_trending_bot',
-    version='0.1.0',
+    version='0.1.1',
     install_requires=[
         'requests==2.12.4',
         'cachetools==2.0.0',
