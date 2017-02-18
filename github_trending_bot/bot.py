1f70b8268539cdca8fa124c0abfe4a7bcd294eeb
aershov <codumentary.com@gmail.com>
2017-02-18 15:19:21 +0300

    refactor TimestampCommand

--- github_trending_bot/bot.py
+++ github_trending_bot/bot.py
@@ -136,10 +136,11 @@ class GithubShowCommand:
 class TimestampCommand:
     def __call__(self, args):
         if not args:
-            return str(dt.datetime.now(dt.timezone.utc).timestamp())
-        date_string = args[0]
-        d_time = dt.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
-        return str(d_time.replace(tzinfo=dt.timezone.utc).timestamp())
+            naive_d_time = dt.datetime.utcnow()
+        else:
+            date_string = args[0]
+            naive_d_time = dt.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
+        return str(naive_d_time.replace(tzinfo=dt.timezone.utc).timestamp())
 
 
 class GithubApi:
